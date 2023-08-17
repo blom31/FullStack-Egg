3397cae85ca51148254697b94e9fc3aa94927d17
Olga Betancourt <blom31@gmail.com>
2023-08-17 20:47:01 -0300

    ejercicio 6 js

---
+++ GUIA 18 JS/js/ejercicio6.js
@@ -0,0 +1,45 @@
+/*identifico la sección a trabajr*/
+const formulario = document.getElementById("formulario");
+
+/*con el boton calcular se llama a la función validar nro ingresado*/
+document.getElementById("calcular").addEventListener("click", validar);
+document.getElementById("calcular").addEventListener("click",validarParImpar);
+/*valido el campo numero ingresado por el usuario*/
+
+function validar(e) {
+  e.preventDefault(); /*evito cualquier evento por defecto*/
+
+  let nro = document.getElementById("nro").value;
+  /*let validar = /^[0-9]+$/; investigar */
+  
+  if (isNaN(parseInt(nro)) || parseInt(nro) < 0) {
+   /* valido que el campo sea numérico*/
+    alert("El valor ingresado no es un valor numérico");
+  } else {
+    if (nro === "") {
+      alert("Ingrese un número");
+    }
+  }
+}
+
+/*validar si el nro ingresado es par o impar*/
+
+function validarParImpar() {
+  let nro = document.getElementById("nro").value;
+  let parimpar = document.getElementById("parimpar");
+  let resto = nro % 2;
+
+ 
+    if (nro == 0) {
+      parimpar.value=("El número 0 no puede ser par ni impar.");
+    
+    }else{
+      if (resto == 0) {
+        parimpar.value = "El número es par";
+      } else {
+        parimpar.value = "El número es impar";
+      }
+    }
+   
+  
+}
